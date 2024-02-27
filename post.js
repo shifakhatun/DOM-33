0d87911196dc9391ffa1597faf33b660e3c1e42e
shifa <shifa.eee@gmail.com>
2024-02-27 15:43:01 +0600

    second commit

--- post.js
+++ post.js
@@ -23,3 +23,39 @@ function postData(posts){
         postContainer.appendChild(postDiv);
     }
 }
+
+function createPost(posts){
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+  method: 'POST',
+  body: JSON.stringify({
+    title: 'foo',
+    body: 'bar',
+    userId: 1,
+  }),
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+  },
+})
+  .then((response) => response.json())
+  .then((json) => console.log(json));
+}
+
+function deletePost(posts){
+    fetch('https://jsonplaceholder.typicode.com/posts/1', {
+  method: 'DELETE',
+});
+}
+
+function patchingPost(posts){
+    fetch('https://jsonplaceholder.typicode.com/posts/1', {
+  method: 'PATCH',
+  body: JSON.stringify({
+    title: 'foo',
+  }),
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+  },
+})
+  .then((response) => response.json())
+  .then((json) => console.log(json));
+}
